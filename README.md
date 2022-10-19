422b0167f303304e24c51a232b83c348fa7e42bc
Michael Fitzgerald <mikitz@users.noreply.github.com>
2022-10-19 22:19:47 +0900

    page all done, just need the data now

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ A website that acts similarly to a video game loading screen for D&amp;D 5e.
 - https://www.reddit.com/r/DnD/comments/a059qp/oc_dd_5e_loading_screen_hints_definitive_edition/
 - https://www.reddit.com/r/d100/comments/em3zf3/quick_d100_dnd_loading_screen_tips/
 - https://www.reddit.com/r/d100/comments/dbil9f/lets_build_100_loading_screen_tips/
+
+
+Custom Fonts: https://www.pagecloud.com/blog/how-to-add-custom-fonts-to-any-website
---
+++ index.js
@@ -0,0 +1,43 @@
+function setIndexListeners(){
+
+}
+// Function to load a local JSON from data/JSONs/
+async function fetchLocalJson(JSON){
+    const response = await fetch(`${JSON}.json`)
+    return data = await response.json()
+}
+
+function randomItemFromObject(object){
+    return object[Math.floor((Math.random()*object.length))];
+}
+
+async function loadingScreen(){
+    const hints = await fetchLocalJson('data')
+    const elementHint = document.getElementById('hint')
+    const elementAuthor = document.getElementById('author')
+
+    let hint = randomItemFromObject(hints)
+    let a = document.createElement('a')
+    a.href = hint.SOURCE
+    a.innerHTML = hint.AUTHOR
+    elementAuthor.appendChild(a)
+
+    let span = document.createElement('span')
+    span.innerHTML = hint.HINT
+    elementHint.appendChild(span)
+
+    setInterval(function(){
+        elementAuthor.innerHTML = ''
+        elementHint.innerHTML = ''
+
+        let hint = randomItemFromObject(hints)
+        let a = document.createElement('a')
+        a.href = hint.SOURCE
+        a.innerHTML = hint.AUTHOR
+        elementAuthor.appendChild(a)
+
+        let span = document.createElement('span')
+        span.innerHTML = hint.HINT
+        elementHint.appendChild(span)
+    }, 30 * 1000)
+}
