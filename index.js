fa197194dd09d09783f33ee74088d4e0881efcbe
Michael Fitzgerald <17684097+mikitz@users.noreply.github.com>
2022-10-20 22:30:25 +0900

    refactoring code

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
-function setIndexListeners(){
-
-}
 // Function to load a local JSON from data/JSONs/
 async function fetchLocalJson(JSON){
     const response = await fetch(`${JSON}.json`)
     return data = await response.json()
 }
-
 function randomItemFromObject(object){
     return object[Math.floor((Math.random()*object.length))];
 }
-
 async function loadingScreen(){
     const hints = await fetchLocalJson('data')
     const elementHint = document.getElementById('hint')
     const elementAuthor = document.getElementById('author')
-
     let hint = randomItemFromObject(hints)
+    updateHint(hint, elementAuthor, elementHint)
+    setInterval(function(){
+        let hint = randomItemFromObject(hints)
+        updateHint(hint, elementAuthor, elementHint)
+    }, 30 * 1000)
+}
+function updateHint(hint, elementAuthor, elementHint){
+    elementAuthor.innerHTML = ''
+    elementHint.innerHTML = ''
+
     let a = document.createElement('a')
     if (hint.SOURCE) a.href = hint.SOURCE
     a.innerHTML = hint.AUTHOR
+    a.target = "_blank"
+    a.rel = "noopener noreferrer"
     elementAuthor.appendChild(a)
 
     let span = document.createElement('span')
     span.innerHTML = hint.HINT
     elementHint.appendChild(span)
 
-    setInterval(function(){
-        elementAuthor.innerHTML = ''
-        elementHint.innerHTML = ''
-
-        let hint = randomItemFromObject(hints)
-        let a = document.createElement('a')
-        if (hint.SOURCE) a.href = hint.SOURCE
-        a.innerHTML = hint.AUTHOR
-        elementAuthor.appendChild(a)
-
-        let span = document.createElement('span')
-        span.innerHTML = hint.HINT
-        elementHint.appendChild(span)
-    }, 30 * 1000)
 }
